3af5ca4e1e021969de7c5b5c4563adfefa6a847d
Ilasbintang <ilasbintang727@gmail.com>
2026-04-06 17:43:47 +0700

    commit 8

--- structNew.cpp
+++ structNew.cpp
@@ -8,3 +8,11 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+}

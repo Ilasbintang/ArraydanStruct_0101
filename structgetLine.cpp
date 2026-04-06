e22a3099a0c1912614a2b30a7f92494fe57c5979
Ilasbintang <ilasbintang727@gmail.com>
2026-04-06 19:02:18 +0700

    commit 14

--- structgetLine.cpp
+++ structgetLine.cpp
@@ -14,3 +14,23 @@ struct Orang {
     int umur;
 };
 
+int main(){
+    Orang mhs;
+    //mengisi data
+    cout << "Mengisi data" << endl;
+    cout << "Nama: ";
+    getline(cin, mhs.nama);
+    cout << "Alamat: ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Kota: ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    //tampilkan 
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama: " << mhs.nama << endl;
+    cout << "Desa: " << mhs.alamat.desa << endl;
+    cout << "Kota: " << mhs.alamat.kota << endl;
+    cout << "Umur: " << mhs.umur << endl;
+     
+}
